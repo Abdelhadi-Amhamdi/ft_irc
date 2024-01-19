b9769ac4aed6436b141efe504bd72af5448bb2a2
Abdelhadi Amhamdi <aamhamdi@e2r2p5.1337.ma>
2024-01-19 15:46:22 +0100

    [refactoring] server

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,129 +6,87 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 20:47:05 by aamhamdi          #+#    #+#             */
-/*   Updated: 2024/01/19 11:15:11 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:32:31 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
-#include "Connection.hpp"
-#include <exception>
-#include <iostream>
-#include <map>
-#include <string>
-#include <sys/_types/_size_t.h>
-#include <sys/poll.h>
-#include <unordered_map>
 
+const std::string& Server::getPassword() const {
+	return (this->password);
+}
 
-void Server::_event() {
-	for (size_t index = 1; index < c_fds.size(); index++) {
-		if ((c_fds[index].revents & POLLIN) == POLLIN) {
-			Connection&	currenConnection = connections[c_fds[index].fd];
-			currenConnection.receiveData();
-			currenConnection.handleDAta(*this);
+void Server::addConnectionFd(const int &connection_fd) {
+	struct pollfd new_fd;
+	new_fd.fd = connection_fd;
+	new_fd.events = POLLIN;
+	connection_fds.push_back(new_fd);
+}
+
+void Server::eventsHandler() {
+	for (size_t index = 1; index < connection_fds.size(); index++) {
+		if ((connection_fds[index].revents & POLLIN) == POLLIN) {
+			Connection *currenConnection = connections[connection_fds[index].fd];
+			currenConnection->receiveDataFromConnection();
+			currenConnection->handleDAta(*this);
 		}
 	}
 }
 
-Server::~Server(){}
-
-
-struct pollfd   getPollfd(int fd)
-{
-	struct pollfd pollFd;
-	pollFd.fd = fd;
-	pollFd.events = POLLIN;
-
-	return pollFd;
-}
-
 Server::Server(const std::string &password, const int &port) 
 	: port(port), password(password) {
-		
-    inializeServer();
-	this->c_fds.push_back(getPollfd(this->server_fd));
-    commands["PASS"] = new Pass();  
-    commands["NICK"] = new Nick();      
-    commands["USER"] = new User();
-    commands["JOIN"] = new Join();      
-    commands["PRIVMSG"] = new PrivMsg();
+	inializeServer();
+	addConnectionFd(this->server_fd);
 }
 
-void Server::add_fd(int fd) {
-    struct pollfd a;
-    a.fd = fd;
-    a.events = POLLIN;
-    c_fds.push_back(a);
-}
-
-bool Server::nickNameused(const std::string &name) {
-    return (cl_manager.checkNickName(name));
-}
-
-void	Server::addUserToChannel(const std::string &channel, const std::string &password, int user_fd, std::string user){
-    ch_manager.addUserToChannel(channel, password, user_fd, user);
-}
-
-
-void Server::start_server()
-{
-	    try
-    {
-        while (true) {
-            if (poll(&this->c_fds[0], this->c_fds.size(), -1) == -1)
-            {
-                std::cerr << "Error on poll.\n";
-                continue; 
-            }
-
-            //if something hapened the mainSockete, then is connection.
-            if (this->c_fds[0].revents & POLLIN)
-            {
-				try{
-					Connection	newConnection(server_fd);
-					connections[newConnection.getFd()] = newConnection;
-					c_fds.push_back(getPollfd(newConnection.getFd()));
+void Server::start_server() {
+	try
+	{
+		while (true) {
+			if (poll(&this->connection_fds[0], this->connection_fds.size(), -1) == -1)
+			{
+				std::cerr << "Error on poll.\n";
+				continue; 
+			}
+			if (this->connection_fds[0].revents & POLLIN)
+			{
+				try {
+					Connection	*newConnection = new Connection(server_fd);
+					connections[newConnection->getFd()] = newConnection;
+					addConnectionFd(newConnection->getFd());
 				}
-                catch (std::exception& e)
-				{
+				catch (std::exception& e) {
 					std::cerr << e.what() << std::endl;
-					continue;
 				}
-            }
-            _event();
-        }
-    } catch (std::exception& e)
-    {
-        std::cerr << "Exception in startServer: " << e.what() << std::endl;
-    }
-}
-
-const std::string& Server::getPassword() const {
-    return password;
+			}
+			eventsHandler();
+		}
+	} catch (std::exception& e) {
+		std::cerr << "Exception in startServer: " << e.what() << std::endl;
+	}
 }
 
 void    Server::inializeServer() {
-    this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->server_fd == -1)
-        throw std::runtime_error("Creation socket failed.");
+	this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (this->server_fd == -1)
+		throw std::runtime_error("Creation socket failed.");
 
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(this->server_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1)
-    {
-        close(this->server_fd);
-        throw std::runtime_error("Error in binding.");
-    }
-
-    if (listen(this->server_fd, SOMAXCONN) == -1)
-    {
-        close(this->server_fd);
-        throw std::runtime_error("Error listening for connections.");
-    }
-
-    std::cout << "Server started on port " << port << std::endl;
+	if (bind(this->server_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1)
+	{
+		close(this->server_fd);
+		throw std::runtime_error("Error in binding.");
+	}
+	if (listen(this->server_fd, SOMAXCONN) == -1)
+	{
+		close(this->server_fd);
+		throw std::runtime_error("Error listening for connections.");
+	}
+	std::cout << GREEN << "Server started listening on port " << port << RESET << std::endl;
 }
+
+Server::~Server() {}
--- src/Server.hpp
+++ src/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 20:46:50 by aamhamdi          #+#    #+#             */
-/*   Updated: 2024/01/19 11:24:52 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:30:57 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
-
-// 
 #include <sys/types.h>
 #include <netdb.h>
 
@@ -31,63 +29,33 @@
 #include "color.hpp"
 #include "ClientSource.hpp"
 #include "ChannelSource.hpp"
-#include "cmd/Join.hpp"
-#include "cmd/PrivMsg.hpp"
-#include "cmd/User.hpp"
 #include "Connection.hpp"
 
-
-
-// containers
+// STL 
+#include <algorithm>
 #include <unordered_map>
 #include <vector>
 #include <map>
 
-// algorithms
-#include <algorithm>
-class Pass;
-#include "cmd/Pass.hpp"
-#include "cmd/Nick.hpp"
 
-class Match {
-	int value;
-	public:
-		Match(int fd) : value(fd) {} 
-		bool operator()(const Client &c) const {
-			return (c.getFd() == value);
-		} 
-};
+class Connection;
 
 class Server {
-    private:
-        const   int port;
-        const   std::string password;
-        int     server_fd;
-        std::vector<struct pollfd> c_fds;
-        std::map<int, Client*> clients;
-        std::unordered_map<int, Connection> connections;
-        std::map<std::string, ACommand*> commands;
-		ClientSource cl_manager;
-		ChannelSource ch_manager;
-    public:
-        Server(const std::string &password, const int &port);
-        void    start_server();
-        void    add_fd(int fd);
-        void    recive_data(int fd);
-        void    _event(int fd);
-        void    new_client(sockaddr *a, socklen_t len, int fd);
-        void    auth(std::string &data, Client &client);
-        const std::string& getPassword() const;
-        const std::map<int, Client*>& getClients() const;
-
-		bool nickNameused(const std::string &name);
-		void addUserToChannel(const std::string &channel, const std::string &password, int user_fd, std::string user);
-        ~Server();
-
-
-        void    inializeServer();
-        
-
-        bool    acceptConnetion();
-        void    _event();
+	private:
+		int		server_fd;
+		const	int port;
+		const	std::string password;
+		std::vector<struct pollfd>			connection_fds;
+		std::unordered_map<int, Client*>	clients;
+		std::unordered_map<int, Connection*>	connections;
+	public:
+		Server(const std::string &password, const int &port);
+		~Server();
+		// server geters
+		const std::string& getPassword() const;
+		// server main functions
+		void    inializeServer();
+		void    eventsHandler();
+		void    start_server();
+		void    addConnectionFd(const int &connection_fd); 
 };
