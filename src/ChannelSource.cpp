00bd13e41a7878a3c1f1b4ab14c1d75dadf98a03
Abdelhadi Amhamdi <aamhamdi@e2r1p3.1337.ma>
2024-01-23 20:25:41 +0100

    update

--- src/ChannelSource.cpp
+++ src/ChannelSource.cpp
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 10:05:23 by aamhamdi          #+#    #+#             */
-/*   Updated: 2024/01/22 09:12:56 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2024/01/23 20:25:23 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,32 +34,5 @@ Channel* ChannelSource::getChannelByName(const std::string &channel_name) {
 		return (channel->second);
 	return (NULL);
 }
-
-void ChannelSource::addUserToChannel(const int &fd, const std::string &user, const std::string channel_name) {
-	std::map<std::string, Channel*>::iterator channel = channels.find(channel_name);
-	if (channel != channels.end()) {
-		channel->second->setAdmin(fd);
-		channel->second->add_user(fd, user);
-	}	
-}
-void ChannelSource::addUserToChannell(const std::string &channel_name, const std::string &password, int user_fd, const std::string &user) {
-	std::map<std::string, Channel*>::iterator channel = channels.find(channel_name);
-	if (channel != channels.end()) {
-		if (channel->second->getKey() != password)
-			throw std::logic_error("ERR_BADCHANNELKEY");
-		channel->second->add_user(user_fd, user);
-	} else {
-		createChannel(channel_name, password);
-		channels[channel_name]->setAdmin(user_fd);
-		channels[channel_name]->add_user(user_fd, user);
-	}
-}
-
-// void ChannelSource::broadcastMeassges(const std::string &channel_name, const std::string &message, std::string user) {
-// 	std::map<std::string, Channel*>::iterator ch = channels.find(channel_name);
-// 	if (ch != channels.end())
-// 		ch->second->brodcast_msg(message, user);
-// }
-
 
 ChannelSource::~ChannelSource(){}
