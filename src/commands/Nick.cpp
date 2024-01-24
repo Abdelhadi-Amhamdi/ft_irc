3cc593256b70d2d2b32e9477534379f8d750c926
Abdelhadi Amhamdi <aamhamdi@e2r2p5.1337.ma>
2024-01-24 17:16:31 +0100

    update Nick message

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:40:05 by aamhamdi          #+#    #+#             */
-/*   Updated: 2024/01/21 20:06:34 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2024/01/24 17:15:43 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 Nick::Nick() : ACommand("Nick") {}
 
+bool Nick::isValidNickname(const std::string &nickname) const {
+    if (nickname.size() > 9)
+        return (false);
+    if (nickname.find("#") != std::string::npos)
+        return (false);
+    if (std::isdigit(nickname[0]) || nickname[0] == ':')
+        return (false);
+    return (true);
+}
+
 void Nick::Execute(std::string &buffer, Connection &user, Server &server) {
     commandFormater(buffer);
     userInfosChecker();
+    if (params.size() && params[0].size() == 1 && params[0].at(0) == ':') {
+        sendResponse(":server_name 431 nick :No nickname given\r\n", user.getFd());
+        return ;
+    }
+    if (!isValidNickname(params[0])) {
+        sendResponse(":server_name 432 nick :Erroneus nickname\r\n", user.getFd());
+        return ;
+    }
     ClientSource *client_manager = server.getClientManager();
     Client *client = client_manager->getClientByNickname(params[0]);
-    
     if (user.getNickname().empty())
     {
         if (!client)
--- src/commands/Nick.hpp
+++ src/commands/Nick.hpp
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:40:01 by aamhamdi          #+#    #+#             */
-/*   Updated: 2024/01/20 18:17:50 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2024/01/24 17:01:54 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 class Client;
 
 class Nick : public ACommand {
-    private:
-        // Client *executer;
+
     public:
         Nick();
+        bool isValidNickname(const std::string &nickname) const;
         void Execute(std::string &buffer, Connection &user, Server &server);
         ~Nick();
 };
