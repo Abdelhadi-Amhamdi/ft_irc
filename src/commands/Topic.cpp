c7d5a9bf51a2d6893c1e52154d58a5dcb353e0f3
Khawla Ben hammou <kben-ham@e2r2p1.1337.ma>
2024-01-29 16:53:44 +0100

    update topic

--- src/commands/Topic.cpp
+++ src/commands/Topic.cpp
@@ -6,7 +6,7 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 11:53:32 by kben-ham          #+#    #+#             */
-/*   Updated: 2024/01/29 17:50:05 by kben-ham         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:53:29 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,73 @@
 // #define ERR_NOTONCHANNEL ": server_name 442 Topic <channel> :You're not on that channel\r\n"
 // #define ERR_NEEDMOREPARAMS  ": server_name 461 <client> <command> :Not enough parameters\r\n"
 // #define ERR_CHANOPRIVSNEEDED  ": server_name 482 <client> <command> :You're not channel operator\r\n"
+// #define ERR_NOSUCHCHANNEL ":server_name 403 Topic <channel> :No such channel\r\n"
+// #define ERR_NOTONCHANNEL ": server_name 442 Topic <channel> :You're not on that channel\r\n"
+// #define ERR_NEEDMOREPARAMS  ": server_name 461 <client> <command> :Not enough parameters\r\n"
+// #define ERR_CHANOPRIVSNEEDED  ": server_name 482 <client> <command> :You're not channel operator\r\n"
 
 Topic::Topic() : ACommand("Topic") {}
 
 void Topic::Execute(std::string &buffer, Connection &user, Server &server)
 {
-    std::string message, new_one;
-    commandFormater(buffer);
-    ChannelSource &channel_manager = server.getChannelManager();
-    if (params[0][0] == '#')
-        params[0].erase(params[0].begin());
-    Channel *tmp = channel_manager.getChannelByName(params[0]);
-    if (tmp == NULL)
-    {
-        message = ":" +  user.getNickname()+" 442 " + user.getNickname() + " #" + params[0] + " :You're not on that channel " + "\r\n";
-        sendResponse(message, user.getFd());
-    }
-    else
-    {
-        int a  = params.size();
-        if (a == 1)
-        {
-            std::string topic = tmp->getTopic();
-            if (topic.empty())
-                message = ":" + user.getNickname() + " 331 "+ user.getNickname() + " #" + params[0] + " :No topic is set.\r\n";
-            else  
-                message = ":" +  user.getNickname()+ " 332 " + user.getNickname() + " #" + params[0] + " " + topic + "\r\n";
-            sendResponse(message, user.getFd());
-        }
-        else if (params[1][0] == ':')
-        {
-            params[1].erase(params[1].begin());
-            //set_topic after check mode (if +t "just operators can change it else throw above")
-            // :irc.example.com 482 dan #v5 :You're not channel operator
-    	    // ERR_CHANOPRIVSNEEDED
-            new_one = params[1];
-            if (a > 1)
-            {
-                for (size_t i = 2; i < params.size(); i++)
-			        new_one +=  " " + params[i];    
-            }
-            tmp->setTopic(new_one);
-            message = ":" +  user.getNickname() + " TOPIC #" + params[0] + " " + new_one + " \r\n";
-            tmp->broadCastResponse(message);
-        }
-        else
-            sendResponse(": server_name 461 <client> <command> :Not enough parameters\r\n", user.getFd());
-        params.clear();
-    }
+	std::string message, topic;
+	commandFormater(buffer);
+	ChannelSource *channel_manager = server.getChannelManager();
+	ClientSource *client_manager = server.getClientManager();
+	Client *tmp_client = client_manager->getClientByNickname(user.getNickname());
+	int a  = params.size();
+	if (params[0] == ":")
+	{
+		sendResponse(":server_name 461 topic :TOPIC Not enough parameters\r\n", user.getFd());
+		return;
+	}
+	if (params[0][0] == '#')
+		params[0].erase(params[0].begin());
+	Channel *tmp = channel_manager->getChannelByName(params[0]);
+	if (tmp == NULL)
+		sendResponse(":server_name 403 Topic #" +params[0] + " :No such channel\r\n", user.getFd());
+	else
+	{
+		std::vector<std::string> k = tmp_client->getgroupsin();
+		std::vector<std::string>::iterator it = std::find(k.begin(), k.end(), params[0]);
+    	if (it == k.end())
+		{
+			sendResponse(":" +  user.getNickname()+" 442 " + user.getNickname() + " #" + params[0] + " :You're not on that channel " + "\r\n", user.getFd());
+			return;
+		}
+		a  = params.size();
+		if (a == 1)
+		{
+			topic = tmp->getTopic();
+			if (topic.empty())
+				message = ":" + user.getNickname() + " 331 "+ user.getNickname() + " #" + params[0] + " :No topic is set.\r\n";
+			else  
+				message = ":" +  user.getNickname()+ " 332 " + user.getNickname() + " #" + params[0] + " :" + topic + "\r\n";
+			sendResponse(message, user.getFd());
+		}
+		else 
+		{
+			if (params[1][0] == ':')
+				params[1].erase(params[1].begin());
+			if (a > 1)
+			{
+				for (size_t i = 2; i < params.size(); i++)
+					params[1] +=  " " + params[i];
+			}
+			ChannelMode channel_mode = tmp->getMode();
+			if (channel_mode.getTopic() == true)
+			{
+				if (tmp->checkIfAdmin(user.getFd()) == false)
+				{
+					sendResponse(":" + user.getNickname() + " 482 "+ user.getNickname() + " #" + params[0] + " :You're not channel operator\r\n", user.getFd());
+					return;
+				}
+			}
+			tmp->setTopic(params[1]);
+			message = ":" +  user.getNickname() + " TOPIC #" + params[0] + " :" + params[1] + " \r\n";
+			tmp->broadCastResponse(message);
+		}
+   }
 }
 
 Topic::~Topic(){}
